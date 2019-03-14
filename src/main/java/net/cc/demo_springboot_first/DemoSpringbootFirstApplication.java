9971128768b5df14934e6ed861628970e6be7cb2
zhizhonghua <zhizhonghua@boco.com.cn>
2019-03-14 09:45:05 +0800

    项目初始化

---
+++ src/main/java/net/cc/demo_springboot_first/DemoSpringbootFirstApplication.java
@@ -0,0 +1,13 @@
+package net.cc.demo_springboot_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootFirstApplication.class, args);
+    }
+
+}
